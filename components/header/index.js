672a38a26c3c00db1384651d436a62bb08f1a882
Ivan Cézanne <ivancezanne.hrzon@gmail.com>
2023-07-01 13:52:15 -0300

    chore(header): removes unused variable

--- components/header/index.js
+++ components/header/index.js
@@ -1,6 +1,6 @@
 import './index.css';
 
-export default (selector) => `<header id="main-header">
+export default () => `<header id="main-header">
     <img alt="qip logo" src="/images/qip-logo.svg" />
     <h1>qip</h1>
   </header>`;
