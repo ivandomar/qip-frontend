71f4a21a983118fc7cf7e925ef4328c7ae827a9b
Ivan Cézanne <ivancezanne.hrzon@gmail.com>
2023-07-01 17:11:42 -0300

    chore(card): populates cards with static data

--- components/content/index.js
+++ components/content/index.js
@@ -1,12 +1,16 @@
 import './index.css';
 import CardComponent from '../card';
 
-const cardTemplate = CardComponent('folder', 1, 'Exemplo 1', 'This is a quite shor note content', '01.07.2023 às 18h45');
+const cardTemplate1 = CardComponent('folder', 1, 'Very importante notes', null, '01.07.2023 às 11h12');
+const cardTemplate2 = CardComponent('folder', 2, 'random thoughts', null, '01.07.2023 às 14h36');
+const cardTemplate4 = CardComponent('note', 4, 'ideas', 'invent some awesome stuff to get rich', '03.07.2023 às 9h18');
+const cardTemplate3 = CardComponent('note', 3, 'shopping list', 'bread and breakfast', '29.06.2023 às 17h05');
+const cardTemplate5 = CardComponent('note', 5, 'to do list', 'buy clothes, write blog, workout, cook dinner, wash the dishes, bath, sleep', '01.07.2023 às 10h39');
 
 export default () => `<section id="main-content">
-    ${cardTemplate}
-    ${cardTemplate}
-    ${cardTemplate}
-    ${cardTemplate}
-    ${cardTemplate}
+    ${cardTemplate1}
+    ${cardTemplate2}
+    ${cardTemplate3}
+    ${cardTemplate4}
+    ${cardTemplate5}
 </section>`;
