d037c5855702f7cdcfa976e73451f6543c04ca9b
Ivan Cézanne <ivancezanne.hrzon@gmail.com>
2023-07-01 15:14:27 -0300

    feature(card): adds stardard elements

--- components/card/index.js
+++ components/card/index.js
@@ -1,3 +1,12 @@
 import './index.css';
 
-export default () => `card`;
+export default () => `<div class="card">
+  <p>card details</p>
+  <p>Card Title</p>
+  <p>card subtitle</p>
+  <div class="card-actions">
+    <button>view</button>
+    <button>edit</button>
+    <button>delete</button>
+  </div>
+</div>`;
