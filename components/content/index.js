189f7e56c83b35d8e3e3f120627f2f70a8da409f
Ivan Cézanne <ivancezanne.hrzon@gmail.com>
2023-07-01 16:26:15 -0300

    feature(content): implements grid layout

--- components/content/index.js
+++ components/content/index.js
@@ -3,4 +3,10 @@ import CardComponent from '../card';
 
 const cardTemplate = CardComponent();
 
-export default () => `<section id="main-content">${cardTemplate}</section>`;
+export default () => `<section id="main-content">
+    ${cardTemplate}
+    ${cardTemplate}
+    ${cardTemplate}
+    ${cardTemplate}
+    ${cardTemplate}
+</section>`;
