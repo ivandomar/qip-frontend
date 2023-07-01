8ea187c9676934c84270ffe908ad78e296d2d19c
Ivan Cézanne <ivancezanne.hrzon@gmail.com>
2023-07-01 16:46:08 -0300

    feature(card): adds enter folder action

--- components/card/index.js
+++ components/card/index.js
@@ -1,18 +1,23 @@
 import './index.css';
 
-export default () => `<div class="card">
-  <p class="details">card details</p>
-  <p class="title">Card Title</p>
-  <p class="subtitle">card subtitlesubtitlesubtitlesubtitlesubtitlesubtitlesubtitlesubtitlesubtitle</p>
-  <div class="card-actions">
-    <button title="view full note content">
-      <span class="material-icons-outlined">visibility</span>
-    </button>
-    <button title="edit note">
-      <span class="material-icons-outlined">edit</span>
-    </button>
-    <button title="delete note" class="card-delete-action">
-      <span class="material-icons-outlined">delete</span>
-    </button>
-  </div>
-</div>`;
+export default (type, id, title, content, details) => {
+  return `<div class="card">
+    <p class="details">${details}</p>
+    <p class="title">${title}</p>
+    <p class="subtitle">${content}</p>
+    <div class="card-actions">
+      <button title="enter folder">
+        <span class="material-icons-outlined">subdirectory_arrow_right</span>
+      </button>
+      <button title="view full note content">
+        <span class="material-icons-outlined">visibility</span>
+      </button>
+      <button title="edit note">
+        <span class="material-icons-outlined">edit</span>
+      </button>
+      <button title="delete note" class="card-delete-action">
+        <span class="material-icons-outlined">delete</span>
+      </button>
+    </div>
+  </div>`;
+};
