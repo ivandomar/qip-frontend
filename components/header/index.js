de137cd7a5a550e00e865756b2c51f1f62323f63
Ivan Cézanne <ivancezanne.hrzon@gmail.com>
2023-07-02 15:39:38 -0300

    feature(main): globalizes button styles

--- components/header/index.js
+++ components/header/index.js
@@ -1,3 +1,8 @@
 import './index.css';
 
-export default () => `<h1 id="logo">qip</h1>`;
+export default () => `
+    <h1 id="logo">qip</h1>
+    <button id="add-button">
+        <span class="material-icons-outlined">add</span>
+    </button>
+`;
