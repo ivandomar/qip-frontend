28791f790ec763511b482f886a817e8c9bd9dc17
Ivan Cézanne <ivancezanne.hrzon@gmail.com>
2023-07-12 22:43:13 -0300

    feature(header): creates dialog for element creation

--- components/header/index.js
+++ components/header/index.js
@@ -1,22 +1,30 @@
 import './index.css';
 import Swal from 'sweetalert2'
 
-// Swal.fire({
-//     title: 'Error!',
-//     text: 'Do you want to continue',
-//     icon: 'error',
-//     confirmButtonText: 'Cool'
-//   })
-
 export default () => {
     const header = document.createElement('div');
+    header.innerHTML = '<h1 id="logo">qip</h1>';
 
-    header.innerHTML = `
-    <h1 id="logo">qip</h1>
-    <button id="add-button">
-        <span class="material-icons-outlined">add</span>
-    </button>
+    // const addFormRef = 
+
+    const addForm = document.createElement('form');
+    addForm.innerHTML = `
+        <input type="radio" name="type" value="1">Folder</input>
+        <input type="radio" name="type" value="2">Note</input>
+        <input type="text" name="title" placeholder="Title" />
+        <input type="text" name="title" placeholder="Title" />
+        <input type="text" name="content" placeholder="Content" />
     `;
+
+    const addButton = document.createElement('button');
+    addButton.id = 'add-button';
+    addButton.innerHTML = '<span class="material-icons-outlined">add</span>';
+    addButton.onclick = () => Swal.fire({
+        title: 'Create new element',
+        html: addForm
+      });
+
+    header.append(addButton);
 
     return header;
 };
