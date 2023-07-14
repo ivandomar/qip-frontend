b368fd2b5dc39b2eba2f7be84e3d8e360f2d5cf1
Ivan Cézanne <ivancezanne.hrzon@gmail.com>
2023-07-13 21:26:28 -0300

    refactor(components): moves creation form to it's own component

---
+++ components/creationForm/index.js
@@ -0,0 +1,68 @@
+import Swal from 'sweetalert2'
+import elementService from '../../services/elementService.';
+
+const handleCreationSubmit = (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+
+    elementService.create(
+        null,
+        Number(formData.get('type')),
+        formData.get('title'),
+        formData.get('content') ? formData.get('content') : null,
+    ).then(({ data: { title } }) => {
+        Swal.fire({
+            icon: 'success',
+            toast: true,
+            timer: 3000,
+            position: 'top',
+            title: `${title} succesfully created`,
+            showConfirmButton: false,
+            background: '#242424',
+            didClose: () => location.reload(),
+        });
+    })
+    .catch((error) => alert(error.message));
+};
+
+const handleFormChange = (form, target) => {
+    if (target.name === 'type') {
+        const contentInput = form.querySelector("input[name='content']");
+
+        if (target.value === '1') {
+            contentInput.removeAttribute('required', false);
+            contentInput.setAttribute('disabled', true);
+        } else if (target.value === '2') {
+            contentInput.setAttribute('required', true);
+            contentInput.removeAttribute('disabled');             
+        }
+    }
+};
+
+export default () => {
+    const form = document.createElement('form');
+    form.id = 'add-form';
+    form.onsubmit = handleCreationSubmit;
+
+    form.onchange = (event) => handleFormChange(form, event.target);
+
+    form.innerHTML = `
+        <label>
+            <input type="radio" name="type" value="1"  required />
+            Folder
+        </label>
+        <label>
+            <input type="radio" name="type" value="2" />
+            Note
+        </label>
+        <br />
+        <input type="text" name="title" placeholder="Title" required />
+        <br />
+        <input type="text" name="content" placeholder="Content" />
+        <br />
+        <button type="submit" id="send-button"><span class="material-icons-outlined">send</span></button>
+    `;
+
+    return form;
+};
--- components/header/index.js
+++ components/header/index.js
@@ -1,75 +1,19 @@
 import Swal from 'sweetalert2'
-import elementService from '../../services/elementService.';
+import creationForm from '../creationForm';
 import './index.css';
 
 export default () => {
     const header = document.createElement('div');
     header.innerHTML = '<h1 id="logo">qip</h1>';
 
-    const addForm = document.createElement('form');
-    addForm.id = 'add-form';
-    addForm.onsubmit = (event) => {
-        event.preventDefault();
-
-        const formData = new FormData(event.target);
-
-        elementService.create(
-            null,
-            Number(formData.get('type')),
-            formData.get('title'),
-            formData.get('content') ? formData.get('content') : null,
-        ).then(({ data: { title } }) => {
-            Swal.fire({
-                icon: 'success',
-                toast: true,
-                timer: 3000,
-                position: 'top',
-                title: `${title} succesfully created`,
-                showConfirmButton: false,
-                background: '#242424',
-                didClose: () => location.reload(),
-            });
-        })
-        .catch((error) => alert(error.message));
-    }
-    addForm.onchange = ({ target }) => {
-        if (target.name === 'type') {
-            const contentInput = addForm.querySelector("input[name='content']");
-
-            if (target.value === '1') {
-                console.log(contentInput, '1');
-                contentInput.removeAttribute('required', false);
-                contentInput.setAttribute('disabled', true);
-            } else if (target.value === '2') {
-                console.log(contentInput, '2');
-                contentInput.setAttribute('required', true);
-                contentInput.removeAttribute('disabled');             
-            }
-        }
-    };
-    addForm.innerHTML = `
-        <label>
-            <input type="radio" name="type" value="1"  required />
-            Folder
-        </label>
-        <label>
-            <input type="radio" name="type" value="2" />
-            Note
-        </label>
-        <br />
-        <input type="text" name="title" placeholder="Title" required />
-        <br />
-        <input type="text" name="content" placeholder="Content" />
-        <br />
-        <button type="submit" id="send-button"><span class="material-icons-outlined">send</span></button>
-    `;
+    const creationFormComponent = creationForm();
 
     const addButton = document.createElement('button');
     addButton.id = 'add-button';
     addButton.innerHTML = '<span class="material-icons-outlined">add</span>';
     addButton.onclick = () => Swal.fire({
         title: 'Create new element',
-        html: addForm,
+        html: creationFormComponent,
         showConfirmButton: false,
       });
 
