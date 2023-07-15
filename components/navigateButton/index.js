972d4da0911ddd0425bb6adec2a86d2392d2ee0d
Ivan Cézanne <ivancezanne.hrzon@gmail.com>
2023-07-14 22:17:25 -0300

    feature(navigateButton): makes it change location stack

--- components/navigateButton/index.js
+++ components/navigateButton/index.js
@@ -1,12 +1,13 @@
-import './index.css';
+import locationService from "../../services/locationService";
 
 const handleClick = (elementId, elementTitle) => {
-    alert(elementId, elementTitle);
+    locationService.navigate(elementId, elementTitle);
+
+    location.reload();
 };
 
 export default (elementId, elementTitle) => {
     const button = document.createElement('button');
-    // button.classList = 'card-view-action';
     button.title = 'enter folder';
     button.innerHTML = '<span class="material-icons-outlined">subdirectory_arrow_right</span>';
     button.onclick = () => handleClick(elementId, elementTitle);
