169e3a0555ced3671f9c6d85c15cfa5389d82ee4
Ivan Cézanne <ivancezanne.hrzon@gmail.com>
2023-07-14 22:03:43 -0300

    feature(breadcrumbs): integrates with location service for breadcrumbs rendering

--- components/breadcrumbs/index.js
+++ components/breadcrumbs/index.js
@@ -1,3 +1,4 @@
+import locationService from '../../services/locationService';
 import './index.css';
 
 export default () => {
@@ -7,35 +8,33 @@ export default () => {
     const backButton = document.createElement('button');
     backButton.id = 'back-button';
     backButton.innerHTML = '<span class="material-icons-outlined">arrow_back_ios</span>';
-    
-    const breadcrumb1 = document.createElement('p');
-    breadcrumb1.id = 'root-breadcrumb';
-    breadcrumb1.classList = 'breadcrumb-item';
-    breadcrumb1.innerHTML = 'root';
 
-    const breadcrumbSeparator1 = document.createElement('p');
-    breadcrumbSeparator1.classList = 'breadcrumb-separator';
-    breadcrumbSeparator1.innerHTML = '>';
-    
-    const breadcrumb2 = document.createElement('p');
-    breadcrumb2.classList = 'breadcrumb-item';
-    breadcrumb2.innerHTML = 'my folder';
-
-    const breadcrumbSeparator2 = document.createElement('p');
-    breadcrumbSeparator2.classList = 'breadcrumb-separator';
-    breadcrumbSeparator2.innerHTML = '>';
-    
-    const breadcrumb3 = document.createElement('p');
-    breadcrumb3.id = 'current-breadcrumb';
-    breadcrumb3.classList = 'breadcrumb-item';
-    breadcrumb3.innerHTML = 'another folder';
+    const rootBreadcrumb = document.createElement('p');
+    rootBreadcrumb.id = 'root-breadcrumb';
+    rootBreadcrumb.classList = 'breadcrumb-item';
+    rootBreadcrumb.innerHTML = 'root';
     
     breadcrumbContainer.append(backButton);
-    breadcrumbContainer.append(breadcrumb1);
-    breadcrumbContainer.append(breadcrumbSeparator1);
-    // breadcrumbContainer.append(breadcrumb2);
-    // breadcrumbContainer.append(breadcrumbSeparator2);
-    breadcrumbContainer.append(breadcrumb3);
+    breadcrumbContainer.append(rootBreadcrumb);
+    
+    const navigationStack = locationService.getStack();
+
+    navigationStack.map((navigationStackItem, index) => {
+        const breadcrumbSeparator = document.createElement('p');
+        breadcrumbSeparator.classList = 'breadcrumb-separator';
+        breadcrumbSeparator.innerHTML = '>';
+
+        const breadcrumb = document.createElement('p');
+        breadcrumb.classList = 'breadcrumb-item';
+        breadcrumb.innerHTML = navigationStackItem.title;
+
+        if (index === locationService.getLastIndex()) {
+            breadcrumb.id = 'current-breadcrumb';
+        }
+        
+        breadcrumbContainer.append(breadcrumbSeparator);
+        breadcrumbContainer.append(breadcrumb);
+    });
 
     return breadcrumbContainer;
 };
